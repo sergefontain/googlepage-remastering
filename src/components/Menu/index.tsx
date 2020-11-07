c038b49012b2b0886bb4b43e456bcda4571c5c7c
sergefontain <sergefontain@gmail.com>
2020-11-08 00:50:40 +0200

    self-closing toggle on click out of the component done

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -8,7 +8,8 @@ const arrObj = [
 ]
 
 const Menu = () => {
-    const [isChecked, setIsChecked] = React.useState(true)
+    const ref: any = React.useRef()
+    //const [isChecked, setIsChecked] = React.useState(true)
     const liPart = arrObj.map((item, index) => (
         <li key={index.toString()}>
             <a href={item.url}>{item.name}</a>
@@ -17,27 +18,34 @@ const Menu = () => {
 
     React.useEffect(() => {
         const changer = (e: any) => {
-            console.log(e)
+            let node = ref.current
+            if (!node.contains(e.target)) {
+                node.children[0].checked = false //setIsChecked(!isChecked)
+            }
         }
         document.addEventListener("click", changer)
-        return (
+        return () => {
             document.removeEventListener("click", changer)
-        )
-    }, [])
+        }
+    },[])
 
     return (
         <div className="container">
             <nav className="main-nav">
-                <input
-                    type="checkbox"
-                    id="menu__toggle"
-                    defaultChecked={!isChecked}
-                />
                 <ul id="mainMenu">
                     {liPart}
-                    <li key={uuidv4()}>
-                        <label className="menu__btn" htmlFor="menu__toggle">
-                            <span></span>
+                    <li key={uuidv4()} ref={ref}>
+                        <input
+                            type="checkbox"
+                            id="menu__toggle"
+                            defaultChecked={false}
+                        />
+                        <label
+                            className="menu__btn"
+                            htmlFor="menu__toggle"
+                            id="label"
+                        >
+                            <span id="labelSpan"></span>
                         </label>
                         <ToggleMenu />
                     </li>
